a7cce6d864dc8b8cba613928ab659b889219741e
Esther Lyon Delsordo <esther.lyondelsordo@speedgoat.io>
2025-08-06 14:44:32 -0600

    Add PHP files

---
+++ objects.php
@@ -0,0 +1,16 @@
+<?php
+// Objects, Classes and Inheritance exercise from learn-php.org
+// Implement the Car class here
+class Car {
+    public function __construct($brand, $year) {
+		$this->brand = $brand;
+        $this->year = $year;
+    }
+    
+    public function print_details() {
+    	echo "This car is a " . $this->year . " " . $this->brand . ".\n";
+    }
+}
+
+$car = new Car("Toyota", 2006);
+$car->print_details();
