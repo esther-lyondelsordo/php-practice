dc0a1b6d9dafd8c93edfa707c74df8ce42fc74b7
Esther Lyon Delsordo <esther.lyondelsordo@speedgoat.io>
2025-08-07 15:08:01 -0600

    Add exceptions file

---
+++ exceptions.php
@@ -0,0 +1,17 @@
+// Exceptions exercise from learn-php.org
+<?php
+# This function will throw an exception!
+function throw_exception() {
+  throw new Exception("Exception!");
+}
+
+# Surround the statement in a try-catch-finally block!
+try {
+    throw_exception();
+} catch (Exception $e) {
+    echo "Exception caught!\n";
+} finally {
+    echo "Done!";
+}
+
+?>
