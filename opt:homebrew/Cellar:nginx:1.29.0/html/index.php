25115577a165a9e19e4d961903a78092602fa8c0
Esther Lyon Delsordo <esther.lyondelsordo@speedgoat.io>
2025-08-12 11:20:52 -0600

    Add vars and throw error on require

--- opt:homebrew/Cellar:nginx:1.29.0/html/index.php
+++ opt:homebrew/Cellar:nginx:1.29.0/html/index.php
@@ -5,6 +5,9 @@
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?>
+        <?php require 'noFileExists.php';
+        echo "I have a $color $car.";
+        ?>
         <?php phpinfo(); ?>
         <?php include 'footer.php';?>
     </body>
